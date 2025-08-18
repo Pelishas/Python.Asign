78669db809a7042077f2477aa96c5f3016934fa2
Elisha <89387338+Pelishas@users.noreply.github.com>
2025-08-18 06:42:27 +0300

    Week 1 Assigment

---
+++ Week1.py
@@ -0,0 +1,17 @@
+# This code performs basic arithmetic operations on two numbers a and b.
+# It calculates the sum, difference, product, quotient, and power of a raised to b
+a=3
+b=4
+
+sum_result = a + b
+diff_result = a - b 
+product_result = a * b
+quotient_result = a / b
+power_result = a ** b
+
+print(sum_result)
+print(diff_result)
+print(product_result)
+print(quotient_result)
+print(power_result) 
+       

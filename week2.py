41b39351d5cd72c7641c76a8c38ccdae1e5ed350
Elisha <89387338+Pelishas@users.noreply.github.com>
2025-08-26 19:48:43 +0300

    week 2 assignment

---
+++ week2.py
@@ -0,0 +1,27 @@
+# Question 1
+my_list = []
+print(my_list)
+
+# Question 2
+my_list.append([10, 20, 30, 40])
+
+# Question 3
+my_list.append(15)
+print(my_list)
+
+# Question 4
+my_list.extend([50,60, 70] )
+print(my_list)
+
+# Question 5
+my_list.remove(70)
+print(my_list)
+
+# Question 6
+my_list= [10, 20, 30, 40, 15, 50, 60]
+my_list.sort()
+print(my_list)
+
+# Question 7
+print(my_list[2])
+
